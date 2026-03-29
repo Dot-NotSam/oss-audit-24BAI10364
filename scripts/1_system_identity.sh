772a3a6e21cf6ace5fd8df7e6f2240e893a01591
Dot-NotSam <sameer.24bai10364@vitbhopal.ac.in>
2026-03-29 15:00:49 +0530

    Create 1_system_identity.sh

---
+++ scripts/1_system_identity.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script 1: System Identity Report
+# Author: Your Name | Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="Your Name"
+SOFTWARE_CHOICE="Git"
+
+# --- System Info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE_TIME=$(date)
+HOME_DIR=$HOME
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | tr -d '"')
+
+# --- Display ---
+echo "========================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "========================================"
+echo "Chosen Software : $SOFTWARE_CHOICE"
+echo "----------------------------------------"
+echo "Distribution    : $DISTRO"
+echo "Kernel Version  : $KERNEL"
+echo "User            : $USER_NAME"
+echo "Home Directory  : $HOME_DIR"
+echo "Uptime          : $UPTIME"
+echo "Date & Time     : $DATE_TIME"
+echo "----------------------------------------"
+echo "License Info    : Linux is typically distributed under the GPL (General Public License)"
+echo "========================================"
