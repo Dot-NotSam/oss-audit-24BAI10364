ddad91175534767889f3cc250185aec3a20dd97f
Dot-NotSam <sameer.24bai10364@vitbhopal.ac.in>
2026-03-29 15:40:55 +0530

    Update 1_system_identity.sh

--- scripts/1_system_identity.sh
+++ scripts/1_system_identity.sh
@@ -13,20 +13,28 @@ USER_NAME=$(whoami)
 UPTIME=$(uptime -p)
 DATE_TIME=$(date)
 HOME_DIR=$HOME
-DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d= -f2 | tr -d '"')
+DISTRO=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
+
+CPU=$(lscpu | grep "Model name" | cut -d: -f2)
+RAM=$(free -h | awk '/Mem:/ {print $2}')
+DISK=$(df -h / | awk 'NR==2 {print $2, $3, $4}')
 
 # --- Display ---
 echo "========================================"
-echo " Open Source Audit — $STUDENT_NAME"
+echo "   Open Source Audit — $STUDENT_NAME"
 echo "========================================"
-echo "Chosen Software : $SOFTWARE_CHOICE"
+echo "Software        : $SOFTWARE_CHOICE"
 echo "----------------------------------------"
-echo "Distribution    : $DISTRO"
-echo "Kernel Version  : $KERNEL"
+echo "OS              : $DISTRO"
+echo "Kernel          : $KERNEL"
 echo "User            : $USER_NAME"
 echo "Home Directory  : $HOME_DIR"
 echo "Uptime          : $UPTIME"
 echo "Date & Time     : $DATE_TIME"
 echo "----------------------------------------"
-echo "License Info    : Linux is typically distributed under the GPL (General Public License)"
+echo "CPU             : $CPU"
+echo "RAM             : $RAM"
+echo "Disk Usage      : $DISK"
+echo "----------------------------------------"
+echo "License         : GNU General Public License (GPL)"
 echo "========================================"
